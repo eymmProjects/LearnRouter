7986f4d5e06bdd07fb579e84cf24f76760d8d166
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2022-11-01 11:16:07 +0800

    Switch, Redirection, 404

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from 'react';
-import {BrowserRouter,Route, Link } from 'react-router-dom';
+import {BrowserRouter,Route, Link, Switch, Redirect } from 'react-router-dom';
 
+import Header from './components/header';
 import Home from './components/home';
 import Post from './components/posts';
 import Profile from './components/profile'
+import PostItem from './components/postitem'
 
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <header>
-          <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-            <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
-            <nav className="my-2 my-md-0 mr-md-3">
-              <Link className="p-2 text-dark" to="/">Home</Link> -
-              <Link className="p-2 text-dark" to="/posts">Posts</Link> - 
-              <Link className="p-2 text-dark" to={{
-                pathname:'/profile',
-                hash:'#francis',
-                search:'?true=enabled'
-              }}>Profile</Link>
-            </nav>
-          </div>
-        </header>
+          <Header/>
         <div className="container">
-          <Route path="/" exact component={Home}/>
-          <Route path="/posts" exact component={Post}/>
-          <Route path="/profile" exact component={Profile}/>
+        <Switch>
+        {/* block */}
+          {/* <Redirect from='profile' to='/' /> */}
+
+
+          <Route path="/posts/:id"  component={PostItem}/>
+          <Route path="/posts"  component={Post}/>
+          <Route path="/profile"  component={Profile}/>
+          <Route path="/" exact  component={Home}/>
+
+          <Route render={()=>(
+            <h3>Opps This page don't exist</h3>
+          )} />
+
+
+        </Switch>
         </div>
     </BrowserRouter>
     </>
---
+++ src/components/postitem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PostsItem = (props)=>{
+    console.log(props)
+    return(
+        <>
+            postsitem
+        </>
+
+    )
+}
+
+export default PostsItem;
