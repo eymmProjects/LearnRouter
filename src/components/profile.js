f01ead97f885ea19261c5363ce440287474baff4
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2022-11-01 09:31:07 +0800

    First Commit

---
+++ src/components/profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Profile = (props)=>{
+    console.log(props);
+    return(
+        <>
+        <Link to={{pathname:`${props.match.url}/posts`}}>posts of profile</Link>
+        </>
+
+
+    )
+}
+
+export default Profile;
